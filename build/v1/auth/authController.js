5e9376e3543c94d9a6354aff2af13e55743f71b6
AhsanZubair1 <87973516+AhsanZubair1@users.noreply.github.com>
2023-08-06 00:06:06 +0500

    Delete build directory
    remove

--- build/v1/auth/authController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.register = function (req, res) {
-    console.log(" vbnm,");
-    var check = "this is a request";
-    var returnPromise = new Promise(function (resolve) { return resolve(check); });
-    return returnPromise;
-};
